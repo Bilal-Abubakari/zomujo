a93281107220b409c2646bab49450e9d031c03e0
= <abubakaribilal99@gmail.com>
2025-03-02 11:03:46 +0000

    feat: implement view patterns and slots (includes actions)

---
+++ src/app/dashboard/settings/_components/timeSlots/createException.tsx
@@ -0,0 +1,184 @@
+import React, { JSX, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { IPatternException } from '@/types/appointment';
+import { requiredStringSchema } from '@/schemas/zod.schemas';
+import { useAppDispatch } from '@/lib/hooks';
+import { createPatternException } from '@/lib/features/appointments/appointmentsThunk';
+import { Toast, toast } from '@/hooks/use-toast';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { CalendarIcon } from 'lucide-react';
+import moment from 'moment';
+import { Calendar } from '@/components/ui/calendar';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { SelectInput, SelectOption } from '@/components/ui/select';
+import { MODE } from '@/constants/constants';
+import { parseTime } from '@/lib/date';
+
+const exceptionTypes: SelectOption[] = [
+  { label: 'Modification', value: 'modification' },
+  { label: 'Cancellation', value: 'cancellation' },
+];
+
+const formSchema = z
+  .object({
+    date: requiredStringSchema(),
+    startTime: requiredStringSchema(),
+    endTime: z.string({
+      required_error: 'End time is required',
+    }),
+    type: requiredStringSchema(),
+    reason: requiredStringSchema(),
+  })
+  .refine(({ endTime, startTime }) => parseTime(endTime) > parseTime(startTime), {
+    path: ['endTime'],
+    message: 'End time must be after start time',
+  });
+
+type CreateExceptionProps = {
+  patternId: string;
+  closeCreateException: () => void;
+};
+
+const CreateException = ({
+  patternId,
+  closeCreateException,
+}: CreateExceptionProps): JSX.Element => {
+  const [isLoading, setLoading] = useState(false);
+  const {
+    control,
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors, isValid },
+  } = useForm<IPatternException>({
+    resolver: zodResolver(formSchema),
+    mode: MODE.ON_TOUCH,
+  });
+  const dispatch = useAppDispatch();
+
+  const onSubmit = async (data: IPatternException): Promise<void> => {
+    setLoading(true);
+    const { startTime, endTime } = data;
+    const { payload } = await dispatch(
+      createPatternException({
+        ...data,
+        patternId,
+        startTime: parseTime(startTime),
+        endTime: parseTime(endTime),
+      }),
+    );
+    toast(payload as Toast);
+    setLoading(false);
+    closeCreateException();
+  };
+
+  return (
+    <div className="mx-auto w-full max-w-md rounded bg-white p-6 shadow">
+      <h2 className="mb-6 text-2xl font-semibold">Create Exception</h2>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <div>
+          <label htmlFor="date" className="mb-1 block text-sm font-medium">
+            Date
+          </label>
+          <div className={cn('grid gap-2')}>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  id="date"
+                  variant={'outline'}
+                  className={cn(
+                    'w-[300px] justify-start text-left font-normal',
+                    !watch('date') && 'text-muted-foreground',
+                  )}
+                  child={
+                    <>
+                      <CalendarIcon />
+                      {watch('date') ? (
+                        moment(watch('date')).format('LL')
+                      ) : (
+                        <span>Pick a date</span>
+                      )}{' '}
+                    </>
+                  }
+                />
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  autoFocus
+                  mode="single"
+                  defaultMonth={new Date()}
+                  startMonth={new Date()}
+                  hidden={[{ before: new Date() }]}
+                  selected={new Date(watch('date'))}
+                  onSelect={(value) =>
+                    value &&
+                    setValue('date', value.toISOString(), {
+                      shouldValidate: true,
+                      shouldTouch: true,
+                    })
+                  }
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4">
+          <Input
+            labelName="Start Time"
+            type="time"
+            error={errors.startTime?.message}
+            {...register('startTime')}
+          />
+          <Input
+            labelName="End Time"
+            type="time"
+            error={errors.endTime?.message}
+            {...register('endTime')}
+          />
+        </div>
+
+        <SelectInput
+          ref={register('type').ref}
+          control={control}
+          options={exceptionTypes}
+          label="Type of Exception"
+          error={errors.type?.message}
+          name="type"
+          placeholder="Select exception type"
+        />
+
+        <Textarea
+          error={errors.reason?.message}
+          labelName="Reason for Exception"
+          {...register('reason')}
+          placeholder="Please provide a reason for this exception..."
+        />
+
+        <div className="flex justify-end gap-4">
+          <Button
+            onClick={closeCreateException}
+            type="button"
+            disabled={isLoading}
+            variant="outline"
+            child={'Cancel'}
+          />
+          <Button
+            disabled={!isValid || isLoading}
+            isLoading={isLoading}
+            child={'Create Exception'}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateException;
--- src/lib/features/appointments/appointmentsThunk.ts
+++ src/lib/features/appointments/appointmentsThunk.ts
@@ -2,12 +2,18 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { IPagination, IQueryParams, IResponse } from '@/types/shared.interface';
 import axios, { axiosErrorHandler } from '@/lib/axios';
 import { Toast } from '@/hooks/use-toast';
-import { ISlot, ISlotPattern, SlotStatus } from '@/types/appointment';
+import {
+  ISlot,
+  ISlotException,
+  ISlotPattern,
+  ISlotPatternBase,
+  SlotStatus,
+} from '@/types/appointment';
 import { generateSuccessToast } from '@/lib/utils';
 
 export const createAppointmentSlot = createAsyncThunk(
   'appointments/createSlot',
-  async (pattern: ISlotPattern): Promise<Toast> => {
+  async (pattern: ISlotPatternBase): Promise<Toast> => {
     try {
       const { data } = await axios.post<IResponse>(`appointments/slot-pattern`, pattern);
       return generateSuccessToast(data.message);
@@ -19,12 +25,68 @@ export const createAppointmentSlot = createAsyncThunk(
 
 export const getAppointmentSlots = createAsyncThunk(
   'appointments/getSlots',
-  async ({ page }: IQueryParams<SlotStatus | ''>): Promise<Toast | IPagination<ISlot>> => {
+  async ({
+    page,
+    status,
+    startDate,
+    endDate,
+  }: IQueryParams<SlotStatus | ''>): Promise<Toast | IPagination<ISlot>> => {
+    const statusQuery = status ? `&status=${status}` : '';
+    const startDateQuery = startDate ? `&startDate=${startDate.toISOString()}` : '';
+    const endDateQuery = endDate ? `&endDate=${endDate.toISOString()}` : '';
     try {
       const { data } = await axios.get<IResponse<IPagination<ISlot>>>(
-        `appointments/slots?page=${page}&orderDirection=asc`,
+        `appointments/slots?page=${page}${statusQuery}${startDateQuery}${endDateQuery}&orderDirection=asc`,
       );
       return data.data;
+    } catch (error) {
+      return axiosErrorHandler(error, true) as Toast;
+    }
+  },
+);
+
+export const deleteSlot = createAsyncThunk(
+  'appointments/deleteSlot',
+  async (id: string): Promise<Toast> => {
+    try {
+      const { data } = await axios.delete<IResponse>(`appointments/slots/${id}`);
+      return generateSuccessToast(data.message);
+    } catch (error) {
+      return axiosErrorHandler(error, true) as Toast;
+    }
+  },
+);
+
+export const deletePattern = createAsyncThunk(
+  'appointments/deletePattern',
+  async (id: string): Promise<Toast> => {
+    try {
+      const { data } = await axios.delete<IResponse>(`appointments/slot-pattern/${id}`);
+      return generateSuccessToast(data.message);
+    } catch (error) {
+      return axiosErrorHandler(error, true) as Toast;
+    }
+  },
+);
+
+export const getSlotPatterns = createAsyncThunk(
+  'appointments/slotPatterns',
+  async (): Promise<Toast | ISlotPattern> => {
+    try {
+      const { data } = await axios.get<IResponse<ISlotPattern>>('appointments/slot-patterns');
+      return data.data;
+    } catch (error) {
+      return axiosErrorHandler(error, true) as Toast;
+    }
+  },
+);
+
+export const createPatternException = createAsyncThunk(
+  'appointments/createPatternException',
+  async (exception: ISlotException): Promise<Toast> => {
+    try {
+      const { data } = await axios.post<IResponse>(`appointments/slot-exception`, exception);
+      return generateSuccessToast(data.message);
     } catch (error) {
       return axiosErrorHandler(error, true) as Toast;
     }
