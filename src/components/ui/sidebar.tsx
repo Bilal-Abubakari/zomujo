b5bc9e75aefb97022663be898dec219ec8aeddf5
= <abubakaribilal99@gmail.com>
2025-03-07 20:11:01 +0000

    build: update tailwind to v4

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -115,7 +115,7 @@ const SidebarProvider = forwardRef<
 
     // We add a state so that we can do data-state="expanded" or "collapsed".
     // This makes it easier to style the sidebar with Tailwind classes.
-    const state = open ? 'expanded' : 'collapsed';
+    // const state = open ? 'expanded' : 'collapsed';
 
     const contextValue = useMemo<SidebarContext>(
       () => ({
