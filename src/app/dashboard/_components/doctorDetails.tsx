d17730d60a1ae9f55cf89010aa6832f8171729d7
Kwabena Owusu Achiaw <kwabena.owusu-achiaw@amalitech.com>
2025-03-07 13:02:07 +0000

    refactor: make showDetails optional

--- src/app/dashboard/_components/doctorDetails.tsx
+++ src/app/dashboard/_components/doctorDetails.tsx
@@ -8,7 +8,7 @@ import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
 
 interface Bookmark extends IDoctor {
-  showBookmark: boolean;
+  showBookmark?: boolean;
 }
 
 const DoctorDetails = ({
