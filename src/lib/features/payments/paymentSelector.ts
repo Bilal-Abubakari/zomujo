e43da4f6135761f86e87d1643efd5f49bd89bd40
= <abubakaribilal99@gmail.com>
2025-03-08 13:17:45 +0000

    feat: use banks from paystack to add payment method

--- src/lib/features/payments/paymentSelector.ts
+++ src/lib/features/payments/paymentSelector.ts
@@ -5,11 +5,6 @@ import { SelectOption } from '@/components/ui/select';
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export const selectPayments = ({ payments }: RootState) => payments;
 
-export const selectIsLoadingBanks = createSelector(
-  selectPayments,
-  ({ isLoadingBank }) => isLoadingBank,
-);
-
 export const selectBanks = createSelector(selectPayments, ({ banks }) => banks);
 
 export const selectBankOptions = createSelector(selectBanks, (banks) =>
