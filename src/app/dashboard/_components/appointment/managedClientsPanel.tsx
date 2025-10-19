a1c8efa5155174f925a897ba0813d9a7aaa2942a
= <abubakaribilal99@gmail.com>
2025-10-19 12:12:25 +0000

    feat: update label from "Managed clients" to "Managed Patients" for improved clarity

--- src/app/dashboard/_components/appointment/managedClientsPanel.tsx
+++ src/app/dashboard/_components/appointment/managedClientsPanel.tsx
@@ -4,7 +4,7 @@ import PatientPanel from '../../(admin)/(user)/_components/patientPanel';
 const ManagedClientsPanel = (): JSX.Element => (
   <div className="mt-4 rounded-lg bg-white">
     <div>
-      <p className="pt-6 pl-6 text-xl font-bold">Managed clients</p>
+      <p className="pt-6 pl-6 text-xl font-bold">Managed Patients</p>
       <PatientPanel doctorView={true} />
     </div>
   </div>
