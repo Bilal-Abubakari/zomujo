c57cf988d17f3ca1c00c49efd35341a15406d136
= <abubakaribilal99@gmail.com>
2025-11-07 23:06:32 +0000

    fix: remove unused medical interfaces to simplify codebase

--- src/lib/features/records/recordsThunk.ts
+++ src/lib/features/records/recordsThunk.ts
@@ -7,13 +7,12 @@ import { ApproveDeclineStatus } from '@/types/shared.enum';
 import { IRecordRequest } from '@/types/appointment.interface';
 import {
   IAllergyRequest,
-  IConditionWithoutId,
   IFamilyMemberRequest,
   IPatient,
   IPatientDataCombined,
   IPatientWithRecord,
-  ISurgeryWithoutId,
 } from '@/types/patient.interface';
+import { IConditionWithoutId, ISurgeryWithoutId } from '@/types/medical.interface';
 import {
   updateAllergies,
   updateConditions,
