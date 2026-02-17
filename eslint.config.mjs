a5211a016e280a6f4a5a5cad68cd06ea94f7f284
Bilal Abubakari <abubakaribilal99@gmail.com>
2026-02-17 21:13:12 +0000

    chore: refactor ESLint configuration to use defineConfig and streamline ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,38 @@
 import eslintJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
 import reactPlugin from 'eslint-plugin-react';
-// import reactHooksPlugin from 'eslint-plugin-react-hooks';
-import nextPlugin from '@next/eslint-plugin-next';
+import nextPlugin from 'eslint-config-next';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
 
-export default tseslint.config(
-  {
-    ignores: [
-      '.next/**',
-      'node_modules/**',
-      'out/**',
-      'build/**',
-      'dist/**',
-      '.cache/**',
-      'public/**',
-      '**/*.config.js',
-      '**/*.config.mjs',
-      '**/*.config.ts',
-      '.eslintrc.js',
-      '.prettierrc.js',
-      'postcss.config.mjs',
-      'jest.config.ts',
-      'tailwind.config.js',
-    ],
-  },
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  globalIgnores([
+    '.next/**',
+    'node_modules/**',
+    'out/**',
+    'build/**',
+    'dist/**',
+    '.cache/**',
+    'public/**',
+    '**/*.config.js',
+    '**/*.config.mjs',
+    '**/*.config.ts',
+    '.eslintrc.js',
+    '.prettierrc.js',
+    'postcss.config.mjs',
+    'jest.config.ts',
+    'tailwind.config.js',
+  ]),
   eslintJs.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ['src/**/*.{js,jsx,ts,tsx}'],
     plugins: {
       react: reactPlugin,
-      // 'react-hooks': reactHooksPlugin,
       '@next/next': nextPlugin,
     },
     rules: {
       ...reactPlugin.configs.recommended.rules,
       ...reactPlugin.configs['jsx-runtime'].rules,
-      // ...reactHooksPlugin.configs.recommended.rules,
       ...nextPlugin.configs.recommended.rules,
       ...nextPlugin.configs['core-web-vitals'].rules,
       'react/react-in-jsx-scope': 'off',
@@ -67,4 +63,6 @@ export default tseslint.config(
       'import/no-cycle': 'off',
     },
   },
-);
+]);
+
+export default eslintConfig;
