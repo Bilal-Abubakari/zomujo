bd6b526e43289e6ed13ddff409cc0dcb04c9e8ae
Bilal Abubakari <abubakaribilal99@gmail.com>
2026-03-07 02:39:02 +0000

    feat: add styling for Investigating appointment status in appointment card

--- src/app/dashboard/_components/appointment/appointmentCard.tsx
+++ src/app/dashboard/_components/appointment/appointmentCard.tsx
@@ -144,6 +144,7 @@ const AppointmentDetails = ({
     [AppointmentStatus.Declined]: 'border-error-300 bg-error-100',
     [AppointmentStatus.Completed]: 'border-green-300 bg-green-100',
     [AppointmentStatus.Progress]: 'border-yellow-300 bg-yellow-100',
+    [AppointmentStatus.Investigating]: 'border-yellow-300 bg-yellow-100',
     [AppointmentStatus.Cancelled]: 'border-error-300 bg-error-100',
     [AppointmentStatus.Incomplete]: 'border-gray-300 bg-gray-100',
   };
